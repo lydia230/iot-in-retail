fb2eb9a026c053f6f1cdbee6a70ad2e68a2ab9a4
lydia <lydia.ay4.la@gmail.com>
2025-10-26 19:48:45 -0400

    FIxed Issues

--- Phase2FrontEnd/index.php
+++ Phase2FrontEnd/index.php
@@ -40,18 +40,18 @@
     </div>
 
 <?php
-	shell_exec("/usr/local/bin/gpio -g mode 27 out");
-	shell_exec("/usr/local/bin/gpio -g mode 23 out");
-	shell_exec("/usr/local/bin/gpio -g mode 12 out");
+	// shell_exec("/usr/local/bin/gpio -g mode 28 out");
+	// shell_exec("/usr/local/bin/gpio -g mode 23 out");
+	// shell_exec("/usr/local/bin/gpio -g mode 12 out");
 
 
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
+	$username = "admin";
+	$password = "123";
 	$dbname = "rfid";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
-
+	
 	if ($conn->connect_error)
 	{
 		die("Connection Failed");
@@ -67,42 +67,41 @@
 
 		if (strlen($phone) < 10 || strlen($phone) > 20) {
 			echo "Invalid phone number! Must contain 10 to 20 numbers!";
-                        shell_exec("/usr/local/bin/gpio -g write 23 1");
-                        shell_exec("/usr/local/bin/gpio -g write 12 1");
-                        shell_exec("/usr/local/bin/gpio -g write 27 0");
-                        sleep(2);
-                        shell_exec("/usr/local/bin/gpio -g write 23 0");
-                        shell_exec("/usr/local/bin/gpio -g write 12 0");
+                        // shell_exec("/usr/local/bin/gpio -g write 23 1");
+                        // shell_exec("/usr/local/bin/gpio -g write 12 1");
+                        // shell_exec("/usr/local/bin/gpio -g write 27 0");
+                        // sleep(2);
+                        // shell_exec("/usr/local/bin/gpio -g write 23 0");
+                        // shell_exec("/usr/local/bin/gpio -g write 12 0");
 			exit;
 		}
 
 		$creationDate = date("Y-m-d H:i:s");
-
+		
 		$sql = "INSERT INTO client (first_name, last_name, email, phone_number, creation_date) VALUES ('$fName','$lName','$email', '$phone', '$creationDate')";
 
 		if ($conn->query($sql) === TRUE)
 		{
 			echo "New record created!";
-			shell_exec("/usr/local/bin/gpio -g write 27 1");
-			shell_exec("/usr/local/bin/gpio -g write 23 0");
-                        shell_exec("/usr/local/bin/gpio -g write 12 0");
-			sleep(2);
-  			shell_exec("/usr/local/bin/gpio -g write 27 0");
+			// shell_exec("/usr/local/bin/gpio -g write 27 1");
+			// shell_exec("/usr/local/bin/gpio -g write 23 0");
+            //             shell_exec("/usr/local/bin/gpio -g write 12 0");
+			// sleep(2);
+  			// shell_exec("/usr/local/bin/gpio -g write 27 0");
 		}
 		else
 		{
 			echo "ERROR: " . $conn->error;
-			shell_exec("/usr/local/bin/gpio -g write 23 1");
-			shell_exec("/usr/local/bin/gpio -g write 12 1");
-			shell_exec("/usr/local/bin/gpio -g write 27 0");
-			sleep(2);
-                        shell_exec("/usr/local/bin/gpio -g write 23 0");
-			shell_exec("/usr/local/bin/gpio -g write 12 0");
+			// shell_exec("/usr/local/bin/gpio -g write 23 1");
+			// shell_exec("/usr/local/bin/gpio -g write 12 1");
+			// shell_exec("/usr/local/bin/gpio -g write 27 0");
+			// sleep(2);
+            //             shell_exec("/usr/local/bin/gpio -g write 23 0");
+			// shell_exec("/usr/local/bin/gpio -g write 12 0");
 
 		}
 
 		$conn->close();
-
 }
 
 ?>
